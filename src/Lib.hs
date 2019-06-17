467ccd3833f9a32a547de8f9fa75e09b8f79390e
Patrik Nygren <patriknygren82@gmail.com>
2019-06-17 23:44:11 +0200

    Draft of game loop mechanics

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,8 +2,76 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Monad.Writer
+import Control.Monad.Reader
+import Control.Monad.Except
+import Control.Monad.Loops (iterateUntilM)
+import Data.Either (isLeft)
+import Prelude as P
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+{--# Plan
+
+Player = the programmable player (handler), contains id, postion and action
+
+Enemy = contains id, position and action
+
+#--}
+
+
+-- Types
+data Position = Pos Int Int
+
+data Direction = Left | Right | Up | Down
+
+data Entity = Entity { life :: Integer
+                     , pos :: Position
+                     , dir :: Direction
+                     }
+
+data Event = ChangeDir Entity Direction
+           | Move Entity Direction
+           | Shoot Entity Direction
+
+data Config = Config { obstacles :: [Position]
+                     , enemies :: [Entity]
+                     , player :: Entity
+                     }
+
+
+-- Combinators
+type Step m a = ReaderT Config (WriterT [Event] m) a
+
+config :: Monad m => Step m Config
+config = ask
+
+push :: Monad m => Event -> Step m ()
+push evt = tell [evt]
+
+runStep :: Monad m => Step m a -> Config -> m (a, [Event])
+runStep step = runWriterT . runReaderT step
+
+
+
+-- Run
+merge :: Monad m => Config -> [Event] -> m Config
+merge conf evs = undefined
+
+ended :: Config -> Bool
+ended conf = undefined
+
+runGame :: Monad m => Step m Config -> Config -> m Config
+runGame step conf =
+  iterateUntilM ended ((=<<) (uncurry merge) . runStep step) conf
+   
+-- runGame step c = do
+  -- e <- runStep step c
+  -- case e of
+    -- P.Left end -> return end
+
 
 
 
