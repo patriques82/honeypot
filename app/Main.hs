456bdc2c190ad5f533d93f03d7f8f10a14061aae
Patrik Nygren <patriknygren82@gmail.com>
2019-06-23 00:14:57 +0200

    Add gloss package with simple simulate example

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
-module Main where
+import Graphics.Gloss
 
-import Lib
+
+
+
+data Model = M (Float, Float)
+
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
+
+window :: Display
+window = InWindow "Pong" (width, height) (offset, offset)
+
 
 main :: IO ()
-main = someFunc
+main = simulate 
+  window
+  white
+  simulationRate
+  initialModel
+  drawingFunc
+  updateFunc
+  where
+    simulationRate :: Int
+    simulationRate = 20
+
+    initialModel :: Model
+    initialModel = M (0,0)
+
+    drawingFunc :: Model -> Picture
+    drawingFunc (M (theta, dtheta)) = Line [(0, 0), (50 * cos theta, 50 * sin theta)]
+
+
+  --    updateFunc :: ViewPort -> Float -> Model -> Model
+    updateFunc _ dt (M (theta, dtheta)) = M (theta + dt * dtheta, dtheta - dt * (cos theta))
