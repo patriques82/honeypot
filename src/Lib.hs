6253cd0c8b751b3bba366cea8497c6a2ddc5e77a
Patrik Nygren <patriknygren82@gmail.com>
2019-07-25 23:31:55 +0200

    Added draft for Extract DSL

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,11 +2,18 @@
 
 module Lib where
 
+import           Control.Arrow        (first, second)
 import           Control.Monad.Reader
 import qualified Data.Map             as M
+import           Data.Monoid          (Sum (..))
+import           Data.Semigroup
+import           Prelude              (Applicative, Bool (..), Bounded, Enum,
+                                       Eq, IO, Int, Maybe (..), Ord, Show, not,
+                                       subtract, undefined, ($), (+), (-), (.),
+                                       (<), (<$>), (>), (>=), (||))
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = undefined
 
 -- Rules
 -- Move, ChangeDir = 1 fuel
@@ -16,8 +23,7 @@ someFunc = putStrLn "someFunc"
 
 
 -- Data Types
-data Pos = Pos Int Int
-  deriving (Eq, Ord)
+type Pos = (Int, Int)
 
 type Dim = Pos
 
@@ -26,21 +32,27 @@ type Fuel = Int
 data Dir = Left | Up | Right | Down
   deriving (Eq, Ord, Enum, Bounded)
 
-data Event = Left -- 1 fuel
-           | Right -- 1 fuel
-           | Move -- 1 fuel
-           | Shoot -- 5 fuel
+data Event = TurnLeft     -- 1 fuel
+           | TurnRight    -- 1 fuel
+           | MoveForward  -- 1 fuel
+           | Shoot        -- 5 fuel
   deriving (Eq, Ord)
 
-newtype Player = Player { _fuel :: Fuel
-                        , _dir  :: Dir 
-                        }
+data Player = Player { fuel :: Fuel
+                     , dir  :: Dir
+                     , pos  :: Pos
+                     }
 
-data Obstacle = Wall | Block | Enemy
+data Cell = Empty | Wall | Block | Enemy
+  deriving Show
 
-data Env = Env { _dim       :: Dim
-               , _obstacles :: Pos -> Obstacle -- M.Map Pos Obstacle
-               , _player    :: Player
+instance Semigroup Cell where
+  Empty <> x = x
+  y     <> _ = y
+
+data Env = Env { dim    :: Dim
+               , cells  :: M.Map Pos Cell
+               , player :: Player
                }
 
 
@@ -64,18 +76,15 @@ env' :: Step Env
 env' = Step ask
 
 dim' :: Step Dim
-dim' = _dim <$> env'
+dim' = dim <$> env'
 
-obstacles' :: Step (M.Map Pos Obstacle)
-obstacles' = _obstacles <$> env'
-
-player' :: Step (Pos, Player)
-player' = _player <$> env'
+player' :: Step Player
+player' = player <$> env'
 
 
 -- Exported
--- Identify obstacle infront
-identify :: Step Obstacle
+-- Identify obstacle in front
+identify :: Step Cell
 identify = undefined
 
 lidarBack :: Step Int
@@ -92,6 +101,53 @@ lidarRight = undefined
 
 
 
+-- DSL for finding cells in map
+type Extract a = Dim -> Pos -> M.Map Pos Cell -> a
+
+-- primitives
+cell :: Extract Cell
+cell d p m = if not (outOfBounds d p)
+                then case M.lookup p m of
+                  Just o  -> o
+                  Nothing -> Empty
+                else Wall
+
+-- helper
+outOfBounds :: Dim -> Pos -> Bool
+outOfBounds (xx,yy) (x,y) =
+  x > xx || x < 0 || y > yy || y < 0
+
+
+
+-- combinators
+up :: Extract a -> Extract a
+up ex d p m = ex d (second (subtract 1) p) m -- Arrow second
+
+eMap :: (a -> b) -> Extract a -> Extract b
+eMap f ext d p m = f $ ext d p m
+
+until :: Semigroup a => Extract a -> (Extract a -> Extract a) -> (Cell -> Bool) -> Extract a
+until ext trans pred d p m =
+  let c = cell d p m
+      found = pred c
+  in if found
+        then ext d p m
+        else ext d p m <> trans (until ext trans pred) d p m
+
+-- helpers
+numerate :: Cell -> Sum Int
+numerate Empty = Sum 1
+numerate _     = Sum 0
+
+notEmpty :: Cell -> Bool
+notEmpty Empty = False
+notEmpty _     = True
+
+
+-- (until cell up notEmpty) d p m = Enemy
+-- (until (eMap numerate cell) up notEmpty = Sum 3
+
+
 
 
 -- provided by user
@@ -101,9 +157,9 @@ playerStep = undefined
 
 -- exported
 frames :: Step Event -> Env -> [Env]
-frames step env = let e = runStep step env
-                  in case resolve env e of
-                    Just env' -> e : frames step env'
+frames step env = let event = runStep step env
+                  in case resolve env event of
+                    Just env' -> env' : frames step env'
                     Nothing   -> []
 
 
