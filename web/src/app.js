053059ccfa13ceeaff723125a065ca84b4ad58f2
Patrik Nygren <patriknygren82@gmail.com>
2020-01-09 21:15:37 +0100

    More webpack stuff

--- web/src/app.js
+++ web/src/app.js
@@ -1,11 +1,13 @@
-console.log("outside");
-window.addEventListener('load', function() {
-   console.log("inside");
-   if (!window.EventSource)
+
+class App {
+  run() {
+    console.log("running");
+    if (!window.EventSource)
         alert("You're browser does not support EventSource needed for this page ");
 
     const eventSource = new EventSource("/start");
     eventSource.onmessage = function(e) {
         console.log("data, ", e.data);
     }
-}, false);
+  }
+}
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/main.js', 
-    output: {
-	filename: 'main.js',
-	path: path.resolve(__dirname, 'dist'),
-    },
+  entry: './src/index.js', 
+  output: {
+	  filename: 'app.bundle.js',
+  	path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
